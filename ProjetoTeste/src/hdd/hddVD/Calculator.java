482448881ffe701d611e7e795e7306ed9905eb78
wellington93 <wellingtonaraujo918@hotmail.com>
2014-05-13 21:12:27 -0300

    Update Calculator.java

--- ProjetoTeste/src/hdd/hddVD/Calculator.java
+++ ProjetoTeste/src/hdd/hddVD/Calculator.java
@@ -1,5 +1,14 @@
 package hdd.hddVD;
 
 public class Calculator {
-
+  
+  private int resultado = 0;
+  
+  private Calculator(int valor){
+    resultado = valor;
+  }
+  
+  public void Calcula(int valor1, double valor2){
+    
+  }
 }
